acd376d854693cacf8ca20a9971dcd2653a22429
imanolarrieta <imanol@stanford.edu>
2015-03-17 12:08:00 -0700

    Add new agents to init file

--- rlpy/Agents/__init__.py
+++ rlpy/Agents/__init__.py
@@ -5,3 +5,5 @@ from .Greedy_GQ import Greedy_GQ
 from .LSPI import LSPI
 from .LSPI_SARSA import LSPI_SARSA
 from .NaturalActorCritic import NaturalActorCritic
+from .PosteriorSampling import PosteriorSampling
+from .UCRL import UCRL
